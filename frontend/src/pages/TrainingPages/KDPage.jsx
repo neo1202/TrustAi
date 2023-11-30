29f969864120b6438bbccfce12bd7efc53eea686
peanut0204 <michellelin9102@gmail.com>
2023-12-01 00:06:15 +0800

    make box disaapear in the KDPage

--- frontend/src/pages/TrainingPages/KDPage.jsx
+++ frontend/src/pages/TrainingPages/KDPage.jsx
@@ -7,6 +7,8 @@ const KDPage = () => {
     const [finishKD, setFinishKD] = useState(false)
     const [finalStudentTestAcc, setFinalStudentTestAcc] = useState(0)
     const [comparison, setComparison] = useState({})
+    const [buttonClicked, setButtonClicked] = useState(false);
+
 
     const handleKD = async () => {
         const response = await fetch(
@@ -35,19 +37,22 @@ const KDPage = () => {
         <p style={{ fontSize: '25px', fontFamily: 'Koulen', textAlign: 'center' }}>{`${finalStudentTestAcc? finalStudentTestAcc:'Not yet trained.'}`}</p>
 
         <br/>
-        <div style={{ display: 'flex', justifyContent: 'center' }}>
-        <div className="p-5 rounded-lg mb-4" style={{ backgroundColor: '#eeeeee', width: '20%', padding: '20px', margin: '20px', display: 'flex', flexDirection: 'column', alignItems: 'center' , boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.3)'}}>
-            <p>Compress your model?</p>
-            <div className="mt-4">
-                <button 
-                className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-3 py-1 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
-                style={{marginRight:'20px'}} onClick={handleKD}>YES</button> 
-                <button 
-                className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-3 py-1 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
-                >NO</button>
+        {!buttonClicked && (
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <div className="p-5 rounded-lg mb-4" style={{ backgroundColor: '#eeeeee', width: '20%', padding: '20px', margin: '20px', display: 'flex', flexDirection: 'column', alignItems: 'center' , boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.3)'}}>
+                <p>Compress your model?</p>
+                <div className="mt-4">
+                    <button 
+                    className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-3 py-1 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
+                    style={{marginRight:'20px'}} onClick={() => { handleKD(); setButtonClicked(true); }}>YES</button> 
+                    <button 
+                    className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-3 py-1 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
+                    onClick={() => setButtonClicked(true)}
+                    >NO</button>
+                </div>
             </div>
-        </div>
-        </div>
+            </div>
+        )}
         <br/>
         <br/>
         
